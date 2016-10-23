35a696d8553f27bee4a3aaeee8004716a697fd6f
ogata-ry <sen_nuki@hotmail.co.jp>
2016-10-23 09:44:12 +0900

    correct refer to review

--- Ex01/Ex01.php
+++ Ex01/Ex01.php
@@ -2,6 +2,13 @@
 /* 
  * Ex01.php
  * 入力した10進数を11進数に変換する
+ *
+ * ●仕様
+ *
+ *  - 半角数字以外が入力されたら入力し直しさせる
+ *  - 空行を入力するとプログラムが終了する
+ *  - 入力数値の行頭に0が1つ以上ある場合は、0が無視される
+ *  - 
  */
 
 require_once(dirname(__FILE__)."/inputData.php");
@@ -9,56 +16,53 @@ require_once(dirname(__FILE__)."/inputData.php");
 /*
  * 入力した10進数を11進数に変換するクラス
  */
-Class Convert {
+Class ValueConverter {
 
     /* 変換のために使う数値 */
-    const VALUE_FOR_CONVERT = "11";
+    const VALUE_FOR_CONVERT = 11;
     /* "A"と表記される数値 */
-    const VALUE_A = "10";
+    const VALUE_A = 10;
     /* 入力上限値 */
-    const INPUT_MAX = "1000000000";
+    const INPUT_MAX = 1000000000;
     /* 入力下限値 */
-    const INPUT_MIN = "1";
+    const INPUT_MIN = 1;
 
     /*
      * メインメソッド
      */
-    static public function main() {
+    public function main() {
+        
+        while (true) {
+            
+            // 処理の説明を表示
+            self::displayDescription();
 
-        // 処理の説明を表示
-        self::displayDescription();
+            // 数値を入力
+            $inputValue = Inputter::inputData();
 
-        // 数値を入力
-        $inputValue = Input::inputValue();
+            // 入力値が空行であればプログラムを終了する
+            if ($inputValue === "") { exit("終了します。\n"); }
 
-        // 0が入力されるまで繰り返し
-        while ($inputValue !== "0") {
+            // 半角数字かどうかを判定する
+            $inputValueIsInt = ctype_digit($inputValue);
 
-            // 入力値が有効かどうか確認する
-            $valueIsValid = self::confirmInputValue($inputValue);
+            // 半角数字以外はやり直し
+            if ($inputValueIsInt === false) { 
+                echo "\n### エラー：半角数字を入力して下さい。 ###\n\n";
+                continue;
+                }
 
-            // 入力値が有効であれば変換する
-            if ($valueIsValid === true) {
+            // 入力数値を整数型に変換する
+            $inputValue = intval($inputValue);
 
-            // 入力値を11進数に変換する
+            // 入力数値を11進数に変換する
             $result = self::convertValue($inputValue);
 
             // 変換結果を表示する
             self::displayResult($inputValue, $result);
 
-            // 処理の説明を表示
-            self::displayDescription();
-
-            } else {
-                echo "\n無効な入力値です。\n";
-                echo "もう一度入力して下さい。\n";
-            }
-
-            // 数値を入力
-            $inputValue = Input::inputValue();
         }
 
-    echo "終了します。\n";
     }
 
     /* 
@@ -67,32 +71,30 @@ Class Convert {
      * @param decimalValue 10進数の数値
      * @return result 11進数の数値
      */
-    static public function convertValue($decimalValue) {
-        $result = ""; // 変換結果
+    public function convertValue($decimalValue) {
 
-        // 入力数値が0より上なら計算
-        if ($decimalValue !== "0") {
+        // 入力数値が0なら変換不要なので0を返す
+        if ($decimalValue === 0) { return $decimalValue; }
 
-            // 10進数を変換する
-            while ($decimalValue !== "0") {
-                // 変換するためにあまりを計算
-                $remain = bcmod($decimalValue, self::VALUE_FOR_CONVERT);
+        $result; // 変換結果
 
-                // 余りが10なら"A"を代入
-                if ($remain === "10") {
-                    $remain = "A";
-                }
+        // 10進数を11進数に変換する
+        while ($decimalValue !== 0) {
 
-                // 変換結果を形成
-                $result = $remain . $result;
+            // 10進数を11で割った剰余を用いて変換する
+            $remain = $decimalValue % self::VALUE_FOR_CONVERT;
 
-                // 変換するために商を計算
-                $decimalValue = bcdiv($decimalValue, self::VALUE_FOR_CONVERT);
+            // 余りが10なら"A"を代入する
+            if ($remain === 10) {
+                $remain = "A";
             }
 
-        } else {
-            // 変換結果に0を代入
-            $result = "0";
+            // 変換結果を形成
+            $result = $remain . $result;
+
+            // 続けて変換をするために11で割った商を変換対象に代入する
+            $decimalValue = intval($decimalValue / self::VALUE_FOR_CONVERT);
+
         }
 
         return $result;
@@ -101,7 +103,7 @@ Class Convert {
     /*
      * 変換結果を表示する
      */
-     static public function displayResult($inputValue, $result) {
+     private function displayResult($inputValue, $result) {
          // 変換結果を表示する
          echo "\n入力した数値（10進数）：{$inputValue}\n";
          echo "変換した数値（11進数）：{$result}\n";
@@ -111,37 +113,39 @@ Class Convert {
      /*
       * 処理の説明を表示する
       */
-      static public function displayDescription() {
+      private function displayDescription() {
         echo "10進数の数値を11進数の数値に変換します。\n";
-        echo "10進数の数値を入力してください。\n";
-        echo "'0'を入力すると終了します。\n";
+        echo "10進数の数値を半角数字で入力してください。\n";
+        echo "空行でエンターを押すと終了します。\n";
       }
 
-    /*
-     * 入力値が有効かどうか確認する
-     * 
-     * @param inputValue 入力値
-     * @return valueIsValid 入力値が有効かどうか
-     */
-    private function confirmInputValue($inputValue) {
-        // 入力値が有効ならtrue
-        $valueIsValid = true;
+    // /*
+    //  * 入力値が有効かどうか確認する
+    //  * 
+    //  * @param inputValue 入力値
+    //  * @return valueIsValid 入力値が有効かどうか
+    //  */
+    //  private function confirmInputValue($inputValue) {
+    //     // 入力値が有効ならtrue
+    //     $valueIsValid = true;
 
-        // 入力値が半角数字かどうか判定する
-        if (!preg_match("/[^0-9]/", $inputValue) === false) {
-            $valueIsValid = false;
-        // 入力値が0から始まるかどうか判定する
-        } else if (!preg_match("/^0/", $inputValue) === false) {
-            $valueIsValid = false;
-        // 入力値(N)が1 ≦ N ≦ 1000000000かどうか判定する
-        } else if (self::INPUT_MIN > $inputValue || self::INPUT_MAX < $inputValue) {
-            $valueIsValid = false;
-        }
+    //     // 入力値が半角数字かどうか判定する
+    //     if (!preg_match("/[^0-9]/", $inputValue) === false) {
+    //         $valueIsValid = false;
+    //     // 入力値が0から始まるかどうか判定する
+    //     } else if (!preg_match("/^0/", $inputValue) === false) {
+    //         $valueIsValid = false;
+    //     // 入力値(N)が1 ≦ N ≦ 1000000000かどうか判定する
+    //     } else if (self::INPUT_MIN > $inputValue || self::INPUT_MAX < $inputValue) {
+    //         $valueIsValid = false;
+    //     }
 
-        return $valueIsValid;
-    }
+    //     return $valueIsValid;
+    // }
 
 }
 
 // プログラム実行
-Convert::main();
+$converter = new ValueConverter();
+
+$converter -> main();
--- Ex01/inputData.php
+++ Ex01/inputData.php
@@ -8,18 +8,42 @@
 /*
  * データを入力するクラス
  */
- Class Input {
+ Class Inputter {
 
     /* 
-     * データを入力し、返す
-     * 
-     * @return inputValue 入力値
+     * 入力されたデータを返す。
+     * 処理の内容を検討しているためコメントアウトが複数存在しています。
+     *
+     * @return 入力値
      */
-    function inputValue() {
-        // 数値を入力する
-        $inputValue = trim(fgets(STDIN));
+    function inputData() {
+
+        // 半角数字が入力されるまで無限ループ
+        // while (true) {
+            // 数値を入力する
+            $inputValue = trim(fgets(STDIN));
+
+            // 空行エンターの場合はそのまま返す(この機能は汎用性が低くなる可能性があるので保留とする)
+            // if ($inputValue === "") { return $inputValue; }
+
+            // // 半角数字であるか確認する
+            // $inputValueIsInt = ctype_digit($inputValue);
+
+            // // 入力値が半角数字であればループを終了する
+            // if ($inputValueIsInt === true) {
+
+            //     // 入力数値をintegerに変換する
+            //     $inputValue = intval($inputValue);
+
+            //     return $inputValue;
+            // } else {
+            //     echo "半角数字を入力して下さい。\n";
+            // }
+
+        // }
 
         return $inputValue;
+
     }
 
  }
