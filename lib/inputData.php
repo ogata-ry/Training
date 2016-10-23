9fdfd1dd3e94079d845fc1cb15735652a1cb9512
ogata-ry <sen_nuki@hotmail.co.jp>
2016-10-23 09:46:08 +0900

    correct indent

--- lib/inputData.php
+++ lib/inputData.php
@@ -30,19 +30,19 @@
         $inputValueIsInt = false; // 入力値が整数かどうか
 
         while ($inputValueIsInt === false) {
-        // 数値を入力する
-        $inputValue = trim(fgets(STDIN));
+            // 数値を入力する
+            $inputValue = trim(fgets(STDIN));
 
-        // 入力値が半角数字かどうか判定する
-        $inputValueIsInt = ctype_digit($inputValue);
-   
-        // 入力値が半角数字であれば入力値を返す。そうでない場合は再入力させる。
-        if ($inputValueIsInt === true) {
-            $inputYear = intval($inputValue);
-            return $inputYear;
-        } else {
-            echo "半角数字で入力して下さい。\n";
-        }
+            // 入力値が半角数字かどうか判定する
+            $inputValueIsInt = ctype_digit($inputValue);
+    
+            // 入力値が半角数字であれば入力値を返す。そうでない場合は再入力させる。
+            if ($inputValueIsInt === true) {
+                $inputYear = intval($inputValue);
+                return $inputYear;
+            } else {
+                echo "半角数字で入力して下さい。\n";
+            }
 
         }
 
